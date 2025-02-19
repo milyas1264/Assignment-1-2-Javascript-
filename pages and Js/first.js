398cc46f1f1d821fd3b61a8f277d657f422bbd74
Muhammad Ilyas <milyasather42401@gmail.con>
2025-02-19 09:04:22 +0500

    edit var

--- pages and Js/first.js
+++ pages and Js/first.js
@@ -1,5 +1,15 @@
 console.log("Hello... I can run JS Through my web browser's console")
 console.log("This is first console for testing")
 console.log("My Javascript learning has been started now")
+console.log("this is the console for testing")
+
+name = "ilyas";
+age = 52; 
+x = 'null';
+y = 'undefined';
+console.log(x);
+console.log(y);
+console.log(name);
+console.log(age);
 
 
